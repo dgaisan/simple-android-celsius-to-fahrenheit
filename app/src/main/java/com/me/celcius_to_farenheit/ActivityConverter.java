81e7c574e615734198a46428937fb974c29b731b
dgaisan <dima.gaisan@gmail.com>
2019-03-09 10:24:13 -0800

    adding converter controls logic

--- app/src/main/java/com/me/celcius_to_farenheit/ActivityConverter.java
+++ app/src/main/java/com/me/celcius_to_farenheit/ActivityConverter.java
@@ -1,13 +1,18 @@
 package com.me.celcius_to_farenheit;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class ActivityConverter extends Activity {
+
+    private TextView resultView;
+    private EditText text;
+    private Boolean isCelsius = true;
 
     @Override
     protected void onCreate(Bundle bundle) {
@@ -18,6 +23,44 @@ public class ActivityConverter extends Activity {
     }
 
     private void initControls() {
+        final Button submit = findViewById(R.id.converter_activity_submit);
+        final RadioGroup radioGroup = findViewById(R.id.converter_radiogroup);
+        text = findViewById(R.id.converter_activity_text_edit);
+        resultView = findViewById(R.id.converter_activity_results);
 
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                isCelsius = group.getCheckedRadioButtonId() == R.id.converter_set_celsius;
+                //if ()
+            }
+        });
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                renderResult();
+            }
+        });
+    }
+
+    private void renderResult() {
+        String textValue = text.getText().toString();
+
+        if (textValue == "" || textValue.isEmpty()) {
+            // No value, nothing to calculate
+            resultView.setText("");
+            return;
+        }
+
+        Double value = Double.valueOf(textValue);
+        String result = "";
+
+        if (isCelsius) {
+           // result = String.valueOf(value )
+        } else {
+
+        }
+        resultView.setText(result);
     }
 }
